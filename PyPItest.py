b1dcb73403f65569ea312860788ed05a3e43aa68
Claire.Liao <41815404+kelly-yt-liao@users.noreply.github.com>
2020-10-15 16:45:32 +0800

    Update PyPItest.py

--- PyPItest.py
+++ PyPItest.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
 
 
     parser = argparse.ArgumentParser(description='PyTorch Testing')
-    parser.add_argument('-img', default=r'/home/Claire/AI/testImg/nevus.jpg',
+    parser.add_argument('--img', default=r'/home/Claire/AI/testImg/nevus.jpg',
                         help='image file to test the skincare level')
-    parser.add_argument('-w', default='/home/Claire/AI/skin/weights/weights.pth',
+    parser.add_argument('--w', default='/home/Claire/AI/skin/weights/weights.pth',
                         help='model weight to test the skincare level')
 
     args = parser.parse_args()
